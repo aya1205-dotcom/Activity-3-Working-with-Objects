331f938246d14f95c32cd46dd19ef42bfa5675d0
angelettecortez <cortezangelette@gmail.com>
2024-10-19 13:39:53 +0800

    Create objectActivity.js

---
+++ objectActivity.js
@@ -0,0 +1,13 @@
+let car = {
+    type: "Sedan",
+    model: "Prius C",
+    color: "Red"
+};
+
+console.log(typeof car);
+
+car.type = "Toyota";
+console.log(car);
+
+car.wheels = 4;
+console.log(car);
